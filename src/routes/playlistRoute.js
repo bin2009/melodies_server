cef4f799d612dd795b0dc0b9054d1b45b024f22e
Hoang Nguyen <freddyfrank69@gmail.com>
2024-10-22 12:24:04 +0700

    Minor fix

--- src/routes/playlistRoute.js
+++ src/routes/playlistRoute.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const playlistController = require('../controllers/playlistController');
 const authMiddleWare = require('../middleware/authMiddleWare');
 
-router.post('/playlists', authMiddleware.verifyToken, playlistController.createPlaylist);
-router.get('/playlists', authMiddleware.verifyToken, playlistController.getAllPlaylists);
-router.get('/playlists/:id', authMiddleware.verifyToken, playlistController.getPlaylist);
-router.put('/playlists/:id', authMiddleware.verifyToken, playlistController.updatePlaylist);
-router.delete('/playlists/:id', authMiddleware.verifyToken, playlistController.deletePlaylist);
-router.post('/playlists/:playlistId/songs', authMiddleware.verifyToken, playlistController.addSongToPlaylist);
-router.delete('/playlists/:playlistId/songs/:songId', authMiddleware.verifyToken, playlistController.removeSongFromPlaylist);
+router.post('/playlists', authMiddleWare.verifyToken, playlistController.createPlaylist);
+router.get('/playlists', authMiddleWare.verifyToken, playlistController.getAllPlaylists);
+router.get('/playlists/:id', authMiddleWare.verifyToken, playlistController.getPlaylist);
+router.put('/playlists/:id', authMiddleWare.verifyToken, playlistController.updatePlaylist);
+router.delete('/playlists/:id', authMiddleWare.verifyToken, playlistController.deletePlaylist);
+router.post('/playlists/:playlistId/songs', authMiddleWare.verifyToken, playlistController.addSongToPlaylist);
+router.delete('/playlists/:playlistId/songs/:songId', authMiddleWare.verifyToken, playlistController.removeSongFromPlaylist);
 
 module.exports = router;
