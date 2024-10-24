9a39bc44ed73c70377fad5121621176bdd7bf860
bin2009 <npbloc.cntt@gmail.com>
2024-10-24 22:40:29 +0700

    Fix db cloud

---
+++ src/config/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port: process.env.DB_PORT,
+        define: {
+            freezeTableName: true,
+        },
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    },
+    test: {
+        username: 'root',
+        password: null,
+        database: 'database_test',
+        host: '127.0.0.1',
+        dialect: 'mysql',
+    },
+    production: {
+        username: 'root',
+        password: null,
+        database: 'database_production',
+        host: '127.0.0.1',
+        dialect: 'mysql',
+    },
+};
